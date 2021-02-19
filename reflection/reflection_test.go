4cbbc8caed8b8e89f9ed8feda36e2f2b9533d867
terenceyak <terenceyak@tutanota.com>
2021-02-19 15:23:07 +0800

    Extend to support nested structs

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	cases := []struct {
 		Name          string
@@ -17,6 +27,30 @@ func TestWalk(t *testing.T) {
 				Name string
 			}{"Chris"},
 			[]string{"Chris"},
+		},
+		{
+			"Struct with two string field",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
 		},
 	}
 
