778e92612a2d6061ee08fc861814e4100644147b
terenceyak <terenceyak@tutanota.com>
2021-03-10 21:06:24 +0800

    Abstract in memory player store into own file

--- json/main.go
+++ json/main.go
@@ -5,20 +5,8 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{
-	store map[string]int
-}
-
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{map[string]int{}}
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
 }
 
 func main() {
