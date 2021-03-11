2f237e66aff53c26564091e5ce1ad22f6cf94073
terenceyak <terenceyak@tutanota.com>
2021-03-11 12:38:44 +0800

    Add NewLeague to remove redundant code

--- io/server_test.go
+++ io/server_test.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
 	"testing"
-	"fmt"
 )
 
 type StubPlayerStore struct {
@@ -170,11 +169,11 @@ func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want s
 
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	t.Helper()
-	err := json.NewDecoder(body).Decode(&league)
+	league, err := NewLeague(body)
 	if err != nil {
 		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
 	}
 
-	return
+	return league
 }
 
