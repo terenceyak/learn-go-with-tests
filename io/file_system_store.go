70ef17588426bdf674c60ade7275bdedc0554ae8
terenceyak <terenceyak@tutanota.com>
2021-03-11 15:41:33 +0800

    Update getLeague to return sorted result by score

--- io/file_system_store.go
+++ io/file_system_store.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
 
 type FileSystemPlayerStore struct {
@@ -31,6 +32,9 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
+	sort.Slice(f.league, func(i, j int) bool {
+		return f.league[i].Wins > f.league[j].Wins
+	})
 	return f.league
 }
 
