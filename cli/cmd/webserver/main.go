6b6110237c9237f3606003a8f07d8df5794ea78e
terenceyak <terenceyak@tutanota.com>
2021-03-12 21:18:27 +0800

    Refactor to open file from path

--- cli/cmd/webserver/main.go
+++ cli/cmd/webserver/main.go
@@ -4,23 +4,17 @@ import (
 	poker "learn-go-with-tests/cli"
 	"log"
 	"net/http"
-	"os"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
-
-	store, err :=   poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
-	}
+	defer close()
 
 	server := poker.NewPlayerServer(store)
 
