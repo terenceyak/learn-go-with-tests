ece104470c083bc839074ca35e9c8ba2801a8559
terenceyak <terenceyak@tutanota.com>
2021-03-12 19:55:26 +0800

    Refactor to use assertPlayerWin helper

--- cli/server_test.go
+++ cli/server_test.go
@@ -86,14 +86,7 @@ func TestStoreWins(t *testing.T) {
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
-
-		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
-		}
+		assertPlayerWin(t, &store, player)
 	})
 
 }
@@ -121,6 +114,18 @@ func TestLeague(t *testing.T) {
 
 	})
 
+}
+
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+	}
 }
 
 func assertStatus(t testing.TB, got, want int) {
